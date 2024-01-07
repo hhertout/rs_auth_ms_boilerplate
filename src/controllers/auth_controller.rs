07801b5df64c573471319b06741402c8c14ace27
hhertout <hugues.hertout@gmail.com>
2024-01-07 22:28:18 +0100

    feat: adding jwt to login method

--- src/controllers/auth_controller.rs
+++ src/controllers/auth_controller.rs
@@ -4,8 +4,8 @@ use axum::Json;
 use serde::{Deserialize, Serialize};
 use crate::api::AppState;
 use crate::controllers::CustomResponse;
-use crate::repository::user_repository::User;
 use crate::services;
+use crate::services::crypto::generate_jwt;
 
 #[derive(Serialize, Deserialize)]
 pub struct LoginBody {
@@ -13,7 +13,13 @@ pub struct LoginBody {
     password: String,
 }
 
-pub async fn login(State(state): State<AppState>, Json(body): Json<LoginBody>) -> Result<Json<User>, (StatusCode, Json<CustomResponse>)> {
+#[derive(Serialize, Deserialize)]
+pub struct LoginResponse {
+    email: String,
+    token: String,
+}
+
+pub async fn login(State(state): State<AppState>, Json(body): Json<LoginBody>) -> Result<Json<LoginResponse>, (StatusCode, Json<CustomResponse>)> {
     let user = match state.repository.get_user_by_email(&body.email).await {
         Ok(user) => user,
         Err(_) => {
@@ -45,5 +51,21 @@ pub async fn login(State(state): State<AppState>, Json(body): Json<LoginBody>) -
             }),
         ));
     }
-    Ok(Json(user))
+
+    let token = match generate_jwt(&user.email) {
+        Ok(t) => t,
+        Err(err) => {
+            return Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(CustomResponse {
+                    message: err.to_string(),
+                }),
+            ))
+        }
+    };
+
+    Ok(Json(LoginResponse {
+        token,
+        email: user.email
+    }))
 }
--- src/services/crypto.rs
+++ src/services/crypto.rs
@@ -1,6 +1,9 @@
+use std::env;
 use argon2::{Argon2, PasswordHash, PasswordHasher, PasswordVerifier};
 use argon2::password_hash::rand_core::OsRng;
 use argon2::password_hash::{Error, SaltString};
+use jsonwebtoken::{encode, EncodingKey, Header, Algorithm};
+use serde::{Deserialize, Serialize};
 
 pub fn hash_password(password: &str) -> Result<String, Error> {
     let salt = SaltString::generate(OsRng);
@@ -14,4 +17,25 @@ pub fn check_password(password: &str, hash: &str) -> Result<bool, Error> {
 
     let argon2 = Argon2::default();
     Ok(argon2.verify_password(password.as_bytes(), &parsed_hash).is_ok())
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Claims {
+    sub: String,
+    exp: u64,
+}
+
+pub fn generate_jwt(email: &str) -> Result<String, jsonwebtoken::errors::Error> {
+    let secret = env::var("JWT_SECRET")
+        .unwrap_or_else(|_| panic!("JWT_SECRET env variable is required"));
+    let my_claims = Claims { sub: email.to_owned(), exp: 3600 * 24 * 20 };
+    let key = secret.as_bytes();
+
+    let header = Header {
+        kid: Some("signing_key".to_owned()),
+        alg: Algorithm::HS512,
+        ..Default::default()
+    };
+
+    encode(&header, &my_claims, &EncodingKey::from_secret(&key))
 }
