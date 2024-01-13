1d34a7545e5bc95157182b4eb6c6a1b27c1dd8fa
hhertout <hugues.hertout@gmail.com>
2024-01-13 21:56:20 +0100

    test: adding role tests

--- tests/specs/access_control_test.rs
+++ tests/specs/access_control_test.rs
@@ -1,10 +1,13 @@
+use std::str::FromStr;
+use auth_api::config::roles::Role;
 use auth_api::services::access_control::{AccessControl, Authorization, GrantAccess};
 
 
 #[test]
+#[should_panic]
 pub fn verify_from_role_test() {
-    let valid_role = vec![String::from("ROLE_USER")];
-    let granted_role = vec![String::from("ROLE_USER")];
+    let valid_role = vec![Role::USER];
+    let granted_role = vec![Role::USER];
 
     let res = match AccessControl::from_role(valid_role, granted_role.clone()) {
         Authorization::Authorized => true,
@@ -12,9 +15,17 @@ pub fn verify_from_role_test() {
     };
     assert!(res);
 
-    let invalid_role = vec![String::from("ROLE_INVALID")];
+    let invalid_role = vec![Role::from_str("INVALID_ROLE").unwrap()];
 
-    let res = match AccessControl::from_role(invalid_role, granted_role) {
+    let res = match AccessControl::from_role(invalid_role, granted_role.clone()) {
+        Authorization::Authorized => true,
+        Authorization::Unauthorized(_) => false
+    };
+    assert!(!res);
+
+    let empty_role = vec![];
+
+    let res = match AccessControl::from_role(empty_role, granted_role.clone()) {
         Authorization::Authorized => true,
         Authorization::Unauthorized(_) => false
     };
